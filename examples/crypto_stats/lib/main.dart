6103ef8e3845b389f04c3bff8f2a513a2be123d4
Elian Ortega <elian.mortega@gmail.com>
2021-09-17 12:59:43 -0600

    feat(crypto_stats): create base project

---
+++ examples/crypto_stats/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:crypto_stats/app/app.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  // Run the app and pass its dependencies to it.
+  runApp(const App());
+}
